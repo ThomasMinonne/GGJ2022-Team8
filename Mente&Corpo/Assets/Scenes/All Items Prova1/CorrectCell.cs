0d5a4551b9de2b47c2bab038aa7764d6bd4a2224
ThomasMinonne <75217586+ThomasMinonne@users.noreply.github.com>
2022-01-29 18:24:56 +0100

    Major changes 2.0

--- Mente&Corpo/Assets/Scenes/All Items Prova1/CorrectCell.cs
+++ Mente&Corpo/Assets/Scenes/All Items Prova1/CorrectCell.cs
@@ -7,6 +7,7 @@ public class CorrectCell : MonoBehaviour
 {
     public bool isCorrect = false;
 	public bool isColliding = false;
+	public bool winningCell = false;
 	public Animator anim;
 	
     // Update is called once per frame
