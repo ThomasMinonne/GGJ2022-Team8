341ec8d81af518fde27f80ba7e49fc1c0aa6e6af
Stefano Capezzuoli <capezzuolistefano5@gmail.com>
2022-01-30 12:43:51 +0100

    NewCursor

---
+++ Mente&Corpo/Assets/DeleteCursor.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using System.Collections;
+
+public class DeleteCursor : MonoBehaviour
+{
+    public Texture2D cursorTexture;
+    public CursorMode cursorMode = CursorMode.Auto;
+    public Vector2 hotSpot = Vector2.zero;
+    void Start()
+    {
+        Cursor.SetCursor(null, Vector3.zero, CursorMode.ForceSoftware);
+    }
+
+
+}
+
