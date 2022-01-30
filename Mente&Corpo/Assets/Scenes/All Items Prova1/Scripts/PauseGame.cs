6600f4c587a41a2401f3b0b0f1ad54a20bcd0a2c
ThomasMinonne <75217586+ThomasMinonne@users.noreply.github.com>
2022-01-30 13:25:05 +0100

    Commit PauseGame

---
+++ Mente&Corpo/Assets/Scenes/All Items Prova1/Scripts/PauseGame.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseGame : MonoBehaviour
+{
+	public GameObject player;
+	public GameObject myPrefab;
+	private GameObject toDestroy;
+	private bool paused = false;
+	
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		if(Input.GetKeyDown(KeyCode.Escape) || Input.GetMouseButtonDown(2)){
+			if(paused){
+				if(Input.GetKeyDown(KeyCode.Escape) || Input.GetMouseButtonDown(2)){
+					SceneManager.LoadScene(0);
+				}
+			}
+			if(!paused){
+				toDestroy = Instantiate(myPrefab, new Vector3(0, 0, 0), Quaternion.identity);
+				//pauseManager.SetActive(true);
+				paused = true;
+			}
+		}
+		
+		if(Input.GetKeyDown(KeyCode.Space)){
+			if(paused){
+				Destroy(toDestroy);
+				//pauseManager.SetActive(false);
+				paused = false;
+			}
+		}
+    }
+}
